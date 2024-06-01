9ce5ad4463fa39f3952d21df3362cc78488851fe
Esteban_Amor <145415808+Estebatron@users.noreply.github.com>
2024-06-01 21:29:13 +0000

    Implemented split in order to separate number and unit of user input

--- main.cs
+++ main.cs
@@ -3,17 +3,17 @@ using System;
 class Program {
   public static void Main (string[] args) {
     
-    Console.WriteLine ("Please enter a number in inches.");
+    Console.WriteLine ("Please enter a number of inches.");
     
-var inches = Console.ReadLine();
+var X = Console.ReadLine();
+    
+var splitX = X.Split(' '); 
+          // splitting the string into two parts, number and unit
+var inches = (splitX[0]);
+         // calling just the value of inches
+    int numberInch;
 
-  char seperator = " "
-  int maxSubstrings = 1;
-    
-var justNumber = inches.Split(seperator, maxSubstrings);
-int numberInch;
-    
-  if(int.TryParse(justNumber, out numberInch)) {
+  if(int.TryParse(inches, out numberInch)) {
 
        Console.WriteLine(numberInch * 2.54 + " centimeters");
     }
